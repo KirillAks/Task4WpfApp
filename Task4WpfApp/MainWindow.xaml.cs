c3dd67e63ae86ecedb61909e50a2259bf6172f06
Пользователь <@DESKTOP-GAKVKQ0>
2022-03-18 16:49:12 +0300

    Добавьте файлы проекта.

---
+++ Task4WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task4WpfApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double rateDollar = Convert.ToDouble(rate.Text);
+            double sumDollar = Convert.ToDouble(sum.Text);
+            double resDouble = rateDollar * sumDollar;
+            resSum.Text = resDouble.ToString();
+        }
+        
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            double rateEuro = Convert.ToDouble(rate1.Text);
+            double sumEuro = Convert.ToDouble(sum1.Text);
+            double resDouble = rateEuro * sumEuro;
+            resSum1.Text = resDouble.ToString();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            double rateGrivna = Convert.ToDouble(rate2.Text);
+            double sumGrivna = Convert.ToDouble(sum2.Text);
+            double resDouble = rateGrivna * sumGrivna;
+            resSum2.Text = resDouble.ToString();
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            double rateDrama = Convert.ToDouble(rate3.Text);
+            double sumDrama = Convert.ToDouble(sum3.Text);
+            double resDouble = rateDrama * sumDrama;
+            resSum3.Text = resDouble.ToString();
+        }
+
+        private void Button_Click_4(object sender, RoutedEventArgs e)
+        {
+            double doubleInch = Convert.ToDouble(inch.Text);
+            double resDouble = doubleInch * 254 / 10000;
+            resInch.Text = resDouble.ToString();
+        }
+
+        private void Button_Click_5(object sender, RoutedEventArgs e)
+        {
+            double doubleFt = Convert.ToDouble(ft.Text);
+            double resDouble = doubleFt * 3048 / 10000;
+            resFt.Text = resDouble.ToString();
+        }
+
+        private void Button_Click_6(object sender, RoutedEventArgs e)
+        {
+            double doubleMile = Convert.ToDouble(mile.Text);
+            double resDouble = doubleMile * 1609344 / 1000;
+            resMile.Text = resDouble.ToString();
+        }
+
+        private void Button_Click_7(object sender, RoutedEventArgs e)
+        {
+            double doubleVerst = Convert.ToDouble(verst.Text);
+            double resDouble = doubleVerst * 10668 / 10;
+            resVerst.Text = resDouble.ToString();
+        }
+    }
+}
